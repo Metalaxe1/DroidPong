9d124914163b0a53bd0b88ccef0144a86fc70d8f
Anthony Ratliff <arratlif@uncg.edu>
2017-07-24 17:02:56 -0400

    Simulated clicking of the single game start button.  Also removed the Head to Head start button.

--- app/src/main/java/com/metalsoft/droidpong/StartGameActivity.java
+++ app/src/main/java/com/metalsoft/droidpong/StartGameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,9 @@ import android.widget.Button;
  */
 
 public class StartGameActivity extends AppCompatActivity {
+    private Button hthButton;
+    private Button singleStart;
+    private Handler handler;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +28,35 @@ public class StartGameActivity extends AppCompatActivity {
             actionBar.setTitle("Droid Pong");
             actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#1946BA")));
         }
+        handler = new Handler();
+        singleStart = (Button) findViewById(R.id.button_single_start);
+        hthButton = (Button) findViewById(R.id.button_hth_start);
+        hthButton.setVisibility(View.GONE);
     }
 
     public void startSingleGameClick(View v){
-        Button singleStart = (Button) v.findViewById(R.id.button_single_start);
         singleStart.setPressed(true);
+        singleStart.setAllCaps(false);
+        singleStart.setBackgroundColor(Color.parseColor("#1946BA"));
+        singleStart.setTextColor(Color.WHITE);
         singleStart.invalidate();
-        //singleStart.setPressed(false);
-        //singleStart.invalidate();
-        Intent intent = new Intent(this, PongActivity.class);
-        startActivity(intent);
+        handler.postDelayed(delayedSingleButtonPress, 250);
     }
 
-    public void startHtHGameClick(View v){
+    private Runnable delayedSingleButtonPress = new Runnable() {
+
+        @Override
+        public void run() {
+            singleStart.setPressed(false);
+            singleStart.setBackgroundColor(Color.WHITE);
+            singleStart.setTextColor(Color.parseColor("#1946BA"));
+            singleStart.invalidate();
+            Intent intent = new Intent(StartGameActivity.this, PongActivity.class);
+            startActivity(intent);
+        }
+    };
+
+        public void startHtHGameClick(View v){
 
     }
 }
